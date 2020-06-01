38b5cb9fda62c95be599f92594acd435a6ee051e
animeshsahu80 <animeshsahu80@gmail.com>
2020-06-01 13:10:07 +0530

    first commit

---
+++ 222_count_complete_tree_nodes.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root==NULL)
+            return 0;
+        int ans=helper(root);
+        
+        return ans;
+    }
+    int helper(TreeNode* root){
+        if(root==NULL)
+            return 0;
+        return 1+ helper(root->left)+helper(root->right);
+    }
+};

93b216d53eb9cf10c32aa5867bb8e22fc09962f9
animeshsahu80 <animeshsahu80@gmail.com>
2020-06-04 23:03:53 +0530

    first commit

---
+++ 234_palindrom_linked_list.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast&&fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        slow=reversed(slow);
+        fast=head;
+        while(slow){
+            if(slow->val!=fast->val)
+                return false;
+            slow=slow->next;
+            fast=fast->next;
+        }
+        return true;
+        
+    }
+    ListNode* reversed(ListNode* head){
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* nxt=head;
+        while(curr){
+            nxt=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+        }
+        return prev;
+    }
+};

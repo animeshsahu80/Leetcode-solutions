2529a5c77f16089153e11b66f07d1128ed1be14f
animeshsahu80 <animeshsahu80@gmail.com>
2020-07-07 02:19:12 +0530

    first commit

---
+++ 714_best_time_to_buy_and_sell_stock_with_transaction_fee.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+       int n=prices.size();
+       vector<int> buy(n);
+       vector<int> sell(n);
+        sell[0]=0;
+        buy[0]=-prices[0];
+        for(int i=1;i<n;i++){
+            buy[i]=max(buy[i-1],sell[i-1]-prices[i]);
+            sell[i]=max(sell[i-1],buy[i-1]+prices[i]-fee);
+        }
+        return sell[n-1];
+    }
+};class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+       int n=prices.size();
+       vector<int> buy(n);
+       vector<int> sell(n);
+        sell[0]=0;
+        buy[0]=-prices[0];
+        for(int i=1;i<n;i++){
+            buy[i]=max(buy[i-1],sell[i-1]-prices[i]);
+            sell[i]=max(sell[i-1],buy[i-1]+prices[i]-fee);
+        }
+        return sell[n-1];
+    }
+};
